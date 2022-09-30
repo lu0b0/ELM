68a59db8de2d29a57699f5c64889e00acebdef51
萝卜 <106028210+lu0b0@users.noreply.github.com>
2022-09-30 17:27:52 +0800

    Add files via upload

--- elm.sh
+++ elm.sh
@@ -19,6 +19,17 @@ else
 	_ftype=""
 fi
 
+download_elm(){
+echo "开始下载elm二进制文件到$PWD/ELM目录"
+curl -sS -o $PWD/ELM/elm-$_ftype --create-dirs https://ghproxy.com/https://raw.githubusercontent.com/lu0b0/ELM/main/ELM/elm-$_ftype
+echo "下载完成，如需重新下载或更新请先删除该文件"
+if [ -f "$PWD/ELM/elm-$_ftype" ]; then
+    echo "$PWD/ELM/elm-$_ftype"
+    eval "chmod +x ./ELM/elm-$_ftype"
+    eval "./ELM/elm-$_ftype -t elm"
+fi
+}
+
 if [ $_ftype == "" ]; then
 	echo "不支持的架构$get_arch"
 else
@@ -27,13 +38,12 @@ else
         echo "$PWD/ELM/elm-$_ftype"
         eval "chmod +x ./ELM/elm-$_ftype"
         eval "./ELM/elm-$_ftype -t elm"
-    else
-        if [ ! -f "$PWD/elm-$_ftype" ]; then
-            echo "在$PWD/ELM目录、$PWD目录下均未找到文件elm-$_ftype"
-            exit 1
-        fi
+    elif [ -f "$PWD/elm-$_ftype" ]; then
         echo "$PWD/elm-$_ftype"
         eval "chmod +x $PWD/elm-$_ftype"
         eval "$PWD/elm-$_ftype -t elm"
+    else
+        echo "在$PWD/ELM目录、$PWD目录下均未找到文件elm-$_ftype，尝试拉取远程仓库文件elm-$_ftype"
+        download_elm
     fi
 fi
