9d4de12f1987d117b278cced41e148af88717df0
萝卜 <106028210+lu0b0@users.noreply.github.com>
2022-10-06 23:54:50 +0800

    Update docker_create.sh

--- images/docker_create.sh
+++ images/docker_create.sh
@@ -56,16 +56,16 @@ if [[ ${CONFIRM} == "Y" || ${CONFIRM} == "y" ]];then
 	DOCKER_UP
 fi
 
-read -p "输入授权码: " sqm
+read -p "\n 输入授权码: " sqm
 sqm=${sqm:-""}
 
-read -p "输入青龙url: 例：http:192.168.0.1" qlurl
+read -p "\n 输入青龙url: 例：（http:192.168.0.1）：" qlurl
 qlurl=${qlurl:-""}
 
-read -p "输入容器CLIENTID: " CLIENTID
+read -p "\n 输入容器CLIENTID: " CLIENTID
 CLIENTID=${CLIENTID:-""}
 
-read -p "输入容器SECRET: " SECRET
+read -p "\n 输入容器SECRET: " SECRET
 SECRET=${SECRET:-""}
 
 echo "{
